5a3b1a2280834c955fb47eec17e96e4fb1ef69d2
Xisca Ayala <148764329+xisca-ayala@users.noreply.github.com>
2024-07-01 10:45:06 +0200

    props

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-const Hero = () => {
+const Hero = ({title}) => {
+
+  const she = { 
+    name:'Maria',
+    age:18};
+
   return (
     <section className='h-[90vh] grid grid-cols-1 md:grid-cols-8'>
       <div className='bg-red-400 md:col-span-5 flex items-center justify-center p-8'>
         <div className=''>
           <h1 className='text-7xl font-bold'>We Design Impactful Digital <span className='text-primary border-8 border-primary p-2'>Products</span></h1>
-          <p className=''>description</p>
-          <p className=''>buttons</p>
+          <p className=''>{she.name}</p>
+          <p className=''> {title} </p>
         </div>
       </div>
       <div className='bg-red-200 md:col-span-3'>img</div>
