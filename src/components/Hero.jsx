7ca3303a7c28ff253276a854db74e37107ad0fa1
Xisca Ayala <148764329+xisca-ayala@users.noreply.github.com>
2024-07-09 11:55:41 +0200

    img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { FaCircle } from "react-icons/fa";
 
 
 const useContador = () => {
@@ -25,19 +26,43 @@ const Hero = ({title}) => {
 
   return (
     <section className='h-[90vh] grid grid-cols-1 md:grid-cols-8'>
-      <div className='bg-red-400 md:col-span-5 flex items-center justify-center p-8'>
-        <div className=''>
-          <h1 className='text-7xl font-bold'>We Design Impactful Digital <span className='text-primary border-8 border-primary p-2'>Products</span></h1>
-          <p className='ml-4 mt-2'>{she.name}</p>
-          <p className='ml-4'> {title} </p>
-          <p className='text-center text-8xl'>{contador}</p>
-          <div className='flex justify-center gap-2 mt-8'>
-            <button onClick={disminuir} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>-</button> 
-            <button onClick={incrementar} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+</button>
+      <div className=' md:col-span-5 flex items-center justify-center p-6'>
+        <div className='p-12'>
+          <h1 className='text-7xl font-bold leading-[6.5rem]'>We Design Impactful Digital {""}
+            <span className='text-primary border-8 border-primary relative py-2 px-6'>Products
+              <FaCircle className='text-white text-base bg-primary rounded-full box-content 
+              absolute -top-5 -left-5 p-2 '/>
+              <FaCircle className='text-white text-base bg-primary rounded-full box-content 
+              absolute -top-5 -right-5 p-2 '/>
+              <FaCircle className='text-white text-base bg-primary rounded-full box-content
+              absolute -bottom-5 -right-5 p-2 '/>
+              <FaCircle className='text-white text-base bg-primary rounded-full box-content
+              absolute -bottom-5 -left-5 p-2 '/>
+            </span>
+            
+          </h1>
+          <div className='pt-6'>
+            <span className='ml-4 mt-2'>{she.name}</span>
+            <span className='ml-4'> {title} </span>
+          </div>
+          <div className='pt-6 p-3'>
+            <button className='bg-primary text-white size-sm rounded p-2'>CONTACT US</button>
+          </div>
+          
+          <div className=' text-center py-10 px-2'>
+              <span className='text-center text-8xl'>{contador}</span>
+              <div className='flex justify-center gap-2 mt-8'>
+                <button onClick={disminuir} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>-</button> 
+                <button onClick={incrementar} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+</button>
+              </div>
           </div>
         </div>
       </div>
-      <div className='bg-red-200 md:col-span-3'>img</div>
+      <div className='md:col-span-3  flex items-center justify-center relative pr-10'>
+        <img src="../src/assets/woman.png" alt="" className=' w-[490px] h-[400px] object-cover -mt-28' />
+        <FaCircle className='text-white text-[370px] bg-primary rounded-full border-[8px] border-primary
+              absolute top-1/2 -translate-y-1/2 -translate-x-1/2 left-1/2 -z-10 '/>
+      </div>
 
     </section>
   )
