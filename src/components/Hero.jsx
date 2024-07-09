868747953dbd7f7542bb47b8577c47e3c3a46f37
Xisca Ayala <148764329+xisca-ayala@users.noreply.github.com>
2024-07-09 12:59:53 +0200

    img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { FaCircle } from "react-icons/fa";
+import { TiStarFullOutline } from "react-icons/ti";
 
 
 const useContador = () => {
@@ -53,17 +54,30 @@ const Hero = ({title}) => {
               <span className='text-center text-8xl'>{contador}</span>
               <div className='flex justify-center gap-2 mt-8'>
                 <button onClick={disminuir} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>-</button> 
-                <button onClick={incrementar} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+</button>
+                <button onClick={incrementar} className='bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+</button>
               </div>
           </div>
         </div>
       </div>
-      <div className='md:col-span-3  flex items-center justify-center relative pr-10'>
-        <img src="../src/assets/woman.png" alt="" className=' w-[490px] h-[400px] object-cover -mt-28' />
+      <div className='md:col-span-3  flex items-center justify-center relative pr-1 h-[70vh]'>
+        <div className='relative'>
+          <img src="../src/assets/woman.png" alt="" className=' w-[490px] h-[400px] object-cover -mt-28' />
+          <div className='bg-white w-[200px] shadow-xl rounded-lg p-4 flex flex-col max-w-[250px] mx-auto gap-2 relative'>
+            <div className='flex relative'>
+              <img src="../src/assets/person2.jpg" alt="" className='w-12 h-12 object-cover rounded-full border-[2px] border-white ' />
+              <img src="../src/assets/person1.jpg" alt="" className='w-12 h-12 object-cover rounded-full absolute left-6 border-[2px]  border-white' />
+            </div>
+            <h2>120 + employees</h2>
+            <div className='flex'>
+              <TiStarFullOutline className='text-primary w-6 h-6 mr-2' />
+              5.0 (3.1k Reviews)
+            </div>
+            <FaCircle className='text-5xl text-primary rounded-full absolute -bottom-[1.3rem] -right-[1.3rem] -z-10'/>
+          </div>
+        </div>
         <FaCircle className='text-white text-[370px] bg-primary rounded-full border-[8px] border-primary
-              absolute top-1/2 -translate-y-1/2 -translate-x-1/2 left-1/2 -z-10 '/>
+        absolute top-1/2 -translate-y-1/2 -translate-x-1/2 left-1/2 -z-10 '/>
       </div>
-
     </section>
   )
 }
